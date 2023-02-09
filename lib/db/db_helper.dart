dada33327949d7845566f2ba3e5c48160aa307c6
sabuj_kumar <sabuj.job21@gmail.com>
2023-02-09 13:15:34 +0600

    dev merge

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,11 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+/*import 'package:cloud_firestore/cloud_firestore.dart';*/
 
 class DbHelper {
-  static const String collectionUser = 'Users';
+/*  static const String collectionUser = 'Users';
   static final _db = FirebaseFirestore.instance;
 
   static Future<bool> isUser(String uid) async {
     final snapshot = await _db.collection(collectionUser).doc(uid).get();
     return snapshot.exists;
-  }
+  }*/
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bye_bye_cry_new/start_page.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
